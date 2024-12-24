23ad00f84aa796bbed5296e439a812bb64cecccb
Алексей <efremovalexei02@gmail.com>
2024-12-25 01:26:50 +0500

    Added more exclamation signs

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Привет мир!");
-        System.out.print("Hello world!");
+        System.out.print("Привет мир!!!");
+        System.out.print("Hello world!!!");
     }
 }
